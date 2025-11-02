596f212327687ea3c25dc8ea9473c4bdeece784a
Gughashree-m <gughashreem@gmail.com>
2025-11-02 09:19:19 +0530

    landing page done

--- src/pages/Landingpage/HeroSection.jsx
+++ src/pages/Landingpage/HeroSection.jsx
@@ -16,15 +16,15 @@ const HeroSection = () => {
           className="filter brightness-[0.9]" // Darken the image for text contrast
         />
       </div>
-      
+
       {/* Content Overlay */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white px-4 text-center">
         <h1 className="text-4xl sm:text-6xl font-serif mb-8 tracking-wider">
           A Memorable Experience.
         </h1>
-        <button className="bg-purple-700 hover:bg-purple-800 text-white font-bold py-3 px-8 rounded-lg shadow-lg transition duration-300 uppercase text-sm tracking-widest">
+        <a href='https://api.whatsapp.com/send?phone=919007062180' target="_blank"><button className="bg-purple-700 hover:bg-purple-800 text-white font-bold py-3 px-8 rounded-lg shadow-lg transition duration-300 uppercase text-sm tracking-widest">
           Reserve Now
-        </button>
+        </button></a>
       </div>
     </div>
   );
